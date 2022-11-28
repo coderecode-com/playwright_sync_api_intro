f0eed6ea9e2ecd3d0465988f0e3ae21daaaa6304
Upendra <eupendra@gmail.com>
2022-11-28 09:56:51 +0530

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 
 def main():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         page.goto('https://quotes.toscrape.com/js')
 
